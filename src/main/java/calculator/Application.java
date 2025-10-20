51bba24edb72e8ddda87ab538cd5d37f9b90ee0c
ndwPC <magma0409@gmail.com>
2025-10-20 20:24:54 +0900

    feat: 빈 문자열일 경우 0 출력하기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,5 +8,9 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
+        if (input.length() == 0) {
+            System.out.println("결과 : 0");
+        }
+
     }
 }
