3b5b2fc69a66b2dd9d61afe4b592be7bf9c78b74
ndwPC <magma0409@gmail.com>
2025-10-20 19:39:28 +0900

    test

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,23 +1,20 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input;
-        Scanner sc = new Scanner(System.in);
-        input = sc.nextLine();
+        String input = readLine();
         if (input.length() == 0) {
             System.out.println("결과 : 0");
         } else if (input.charAt(0) == '/' && input.charAt(1) == '/') {
             int idx = input.indexOf("\\n");
-            System.out.println(idx);
-            System.out.println(input.substring(2, idx));
             String custom = ",|:";
             for (char c : input.substring(2, idx).toCharArray()) {
                 custom += "|" + c;
             }
-            System.out.println(input.substring(idx + 2));
             String arr[] = input.substring(idx + 2).split(custom);
             int num = 0;
             for (String cut : arr) {
@@ -26,7 +23,6 @@ public class Application {
                 } catch (NumberFormatException e) {
                     throw new IllegalArgumentException("잘못된 입력");
                 }
-                System.out.println(cut);
             }
             System.out.println("결과 : " + num);
         } else {
@@ -38,7 +34,6 @@ public class Application {
                 } catch (NumberFormatException e) {
                     throw new IllegalArgumentException("잘못된 입력");
                 }
-                System.out.println(cut);
             }
             System.out.println("결과 : " + num);
         }
