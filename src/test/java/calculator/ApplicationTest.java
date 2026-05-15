6a3c7e61f2b578795509c1d69d70a8f96b9a6852
ndwPC <magma0409@gmail.com>
2026-05-15 20:16:58 +0900

    test: 커스텀 구분자 단일 숫자 케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,6 +17,14 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void test() {
+        assertSimpleTest(() -> {
+            run("//:,\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
